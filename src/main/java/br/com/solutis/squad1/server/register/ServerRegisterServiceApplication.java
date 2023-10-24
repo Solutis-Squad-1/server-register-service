68c8f074b41140c901a71f09b12ad3213d00ac7d
Victor Kayk <victorkayk77@gmail.com>
2023-10-24 11:42:41 -0300

    initial commit

---
+++ src/main/java/br/com/solutis/squad1/server/register/ServerRegisterServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.solutis.squad1.server.register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerRegisterServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerRegisterServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/solutis/squad1/server/register/ServerRegisterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.solutis.squad1.server.register;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerRegisterServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
